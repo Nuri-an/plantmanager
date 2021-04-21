ff8700e3c102cc0a463494ef220654374cc12578
Nurian Coelho <nuriancoelho@hotmail.com>
2021-04-21 19:49:38 -0300

    Rafatorando código

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { Welcome } from './src/pages/Welcome';
 
+import { useFonts, Jost_400Regular, Jost_700Bold } from '@expo-google-fonts/jost';
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_700Bold
+  })
+
+  if(!fontsLoaded) { //enquanto a font não foi carregada, segura a tela de splash do app
+    return <AppLoading />
+  }
+
   return (
     <Welcome />
   );
